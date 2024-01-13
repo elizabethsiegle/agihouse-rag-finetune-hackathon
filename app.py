0aa129a55eaea9a516316949766180c2273eabb1
elizabethsiegle <lsiegle@twilio.com>
2024-01-13 13:45:36 -0800

    hi sarah p2 load_dotenv

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+from dotenv import load_dotenv
 from metaphor_python import Metaphor
+import requests
 import streamlit as st
+
+metaphor = Metaphor(os.environ.get("METAPHOR_API_KEY"))
+load_dotenv()
 
 st.title("Hackathon product name")
 site = st.text_input("Link to company site")
 if st.button('Generate competitors report'):
-    
+
 # st.text_input("Email to send report to")
