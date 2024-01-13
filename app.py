cf1f28e29e034cf70bf9c85ce6432d9328bb5ee6
elizabethsiegle <lsiegle@twilio.com>
2024-01-13 13:34:08 -0800

    hi sarah

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+from metaphor_python import Metaphor
 import streamlit as st
 
 st.title("Hackathon product name")
+site = st.text_input("Link to company site")
+if st.button('Generate competitors report'):
+    
+# st.text_input("Email to send report to")
