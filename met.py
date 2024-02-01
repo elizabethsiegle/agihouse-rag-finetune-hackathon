d8cc376817e59fb822b6abea2c2efebcffa6ae24
elizabethsiegle <lsiegle@twilio.com>
2024-02-01 14:14:10 -0800

    blog branch

--- met.py
+++ met.py
@@ -1,11 +1,16 @@
-from metaphor_python import Metaphor
+from dotenv import dotenv_values
+from exa_py import Exa
+import os
 import requests
 
-METAPHOR_API_KEY = "2e22c147-fe26-4934-8c1f-a82a834afafd"
-# given an idea, returns a list of competitors 
+config = dotenv_values(".env")
+EXA_API_KEY=
+exa = Exa(api_key=EXA_API_KEY)
+
+# given an idea, returns a list of competitors including snippets
 def find_competitors(idea):
     search = "Here's my company idea, " + idea + ". Here's a company that's doing it:"
-    url = "https://api.metaphor.systems/search/snippet"
+    url = "https://api.exa.ai/search/snippet"
 
     payload = { "query": search,
             "snippetLength": 3,
@@ -16,63 +21,56 @@ def find_competitors(idea):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "x-api-key": METAPHOR_API_KEY
+        "x-api-key": EXA_API_KEY
     }
 
     response = requests.post(url, json=payload, headers=headers)
     response_json = response.json()
     results = response_json['results']
+    # print(f'results {results}')
     return results
 
 #given a company url, returns websites + document IDs about the company
-def research_company(company_link):
-    url = "https://api.metaphor.systems/search"
+# def research_company(company_link):
+#     url = "https://api.exa.ai/search"
 
-    payload = {
-        "query": company_link,
-        "type": "keyword",
-        "numResults": 2,
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "x-api-key": METAPHOR_API_KEY
-    }
+#     payload = {
+#         "query": company_link,
+#         "type": "keyword",
+#         "numResults": 2,
+#     }
+#     headers = {
+#         "accept": "application/json",
+#         "content-type": "application/json",
+#         "x-api-key": EXA_API_KEY
+#     }
 
-    response = requests.post(url, json=payload, headers=headers)
-    response_json = response.json()
-    results = response_json['results']
-    return results
+#     response = requests.post(url, json=payload, headers=headers)
+#     response_json = response.json()
+#     results = response_json['results']
+#     return results
 
-def get_contents(document_id):
-    base_url = "https://api.metaphor.systems/contents"
-    url = f"{base_url}?ids={document_id}"
-    headers = {
-        "accept": "application/json",
-        "x-api-key": METAPHOR_API_KEY
-    }
-
-    response = requests.get(url, headers=headers)
-    response_json = response.json()
-    results = response_json['contents'][0]["extract"]
-    return results
-
-def whodoneit(idea):
-    #find the top 10 competitors
-    competitors = {}
-    competitor_results = find_competitors(idea)
-    for competitor in competitor_results:
-        
-        #for each competitor, look up 5 links
-        competitor_contents = []
-        competitor_contents.append(competitor['snippet'])
-        competitor_research = research_company(competitor['url'])
-        for link in competitor_research:
-            competitor_contents.append(get_contents(link['id']))
-        competitors[competitor['url']] = competitor_contents
-    print(competitors)
+def find_similar_and_contents_url(idea): 
+    search_response = exa.find_similar_and_contents(
+        idea,
+        highlights={"num_sentences":2},
+        num_results=10)
+    competitors = search_response.results
+    print(f'competitors {competitors}')
+    urls = {}
+    for c in competitors:
+        print(f'{c.title}: {c.url}')
     return competitors
 
+    # print(f"comp {competitors[0]}") 
+
 idea = "women's tennis clothing"
-print(find_competitors(idea))
-#whodoneit(idea)
+# print(f'find_competitors(idea){find_competitors(idea)}')
+# find_sim = []
+# for i in find_competitors(idea):
+#     x = find_similar_and_contents_url(i['url'])
+#     find_sim.append(x)
+# print(f'find_sim {find_sim}')
+# print(find_competitors(idea)[0]['url'])
+
+
